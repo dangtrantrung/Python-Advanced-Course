7cc4aff20d009266a715aa0dfd3e1e70d0e4df7b
dangtrantrung <trung280584@gmail.com>
2024-02-17 15:04:59 +0700

    default, requrired,const, dest

--- arguments_parse.py
+++ arguments_parse.py
@@ -9,8 +9,8 @@ parser.add_argument("y",type=int)
 
 ''' Optional arguments '''
 # parse args
-parser.add_argument("--x1",type=int)
-parser.add_argument("--y1",type=int)
+parser.add_argument("--x1",type=int,default=100)
+parser.add_argument("--y1",type=int,required=True)
 # get_info
 args=parser.parse_args()
 print("x= ",args.x)
