e5953f2debd0b5b239568cf0959dcb7c15932a1e
dangtrantrung <trung280584@gmail.com>
2024-02-17 11:41:42 +0700

    JSON

---
+++ JSONfile.py
@@ -0,0 +1,19 @@
+import json  # same as python dictionary obj
+
+dict={
+    'name':"Trung",
+    'age': 40,
+    'hobbies': ['sleeping','ping pong', 'dev', 'eating']
+    }
+
+# convert python dict to JSON string
+json_string=json.dumps(dict)
+print(type(json_string))
+print(json_string)
+# convert json string to python object
+new_dict=json.loads(json_string)
+print(type(new_dict))
+print(new_dict)
+print(new_dict['hobbies'])
+for hb in new_dict['hobbies']:
+    print(hb)
