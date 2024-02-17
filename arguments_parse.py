663603a5454c7ef2380e6609d8f73084dcc1c370
dangtrantrung <trung280584@gmail.com>
2024-02-17 13:17:07 +0700

    ArgumentParser

---
+++ arguments_parse.py
@@ -0,0 +1,21 @@
+import argparse
+
+# create parser
+parser=argparse.ArgumentParser()
+""" Positional arguments """
+# parse args
+parser.add_argument("x",type=int)
+parser.add_argument("y",type=int)
+
+''' Optional arguments '''
+# parse args
+parser.add_argument("--x1",type=int)
+parser.add_argument("--y1",type=int)
+# get_info
+args=parser.parse_args()
+print("x= ",args.x)
+print("y= ",args.y)
+print("Sum= ", args.x+args.y)
+print("x1= ",args.x1)
+print("y1= ",args.y1)
+print("Sum_opt= ", args.x1+args.y1)
