f857c17ca75efcf0848a99482573ee51091793dc
dangtrantrung <trung280584@gmail.com>
2024-02-17 15:12:11 +0700

    sort list

--- lambda_function.py
+++ lambda_function.py
@@ -4,3 +4,9 @@ print(my_func(4,6))
 
 my_func1=lambda x,y: x+y
 print(my_func1(3,4))
+
+# sort list
+lst=["Huy", "hoi dan IT","CodeXplore","Coding AI", "Mi AI","Trung AI"]
+print(sorted(lst))
+print(sorted(lst,key=lambda x:len(x)))
+
