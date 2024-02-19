1302644a7fe365e4ba4eb575f3c28f6f72e408ab
dangtrantrung <trung280584@gmail.com>
2024-02-19 22:20:13 +0700

    try, except

--- network_urllib.py
+++ network_urllib.py
@@ -1,27 +1,35 @@
-import urllib.request
-import urllib.parse
 import ssl
+import urllib.parse
+import urllib.request
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url_google=urllib.request.urlopen("https://google.com")
-print(url_google.read())
+try:
+    url_google=urllib.request.urlopen("https://google.com")
+    print(url_google.read())
+    # 2
+    print('----------INFO---------')
+    print(url_google.info())
+    print(url_google.geturl())
 
-# 2
-print('----------INFO---------')
-print(url_google.info())
-print(url_google.geturl())
+    print('------END-----')
+except urllib.error.URLError as e:
+    print('Error', e.reason)
 
-print('------END-----')
-
-url_python=urllib.request.urlopen("https://www.python.org/search/?q=urlopen&submit=Search")
-print(url_python.read())
-
+try:
+    url_python=urllib.request.urlopen("https://www.python.org/search/?q=urlopen&submit=Search")
+    print(url_python.read())
+except urllib.error.URLError as e:
+    print('Error', e.reason)
 
 url='https://www.python.org/search'
 values={'q':'basic','submit':'search'}
 data=urllib.parse.urlencode(values)
 data=data.encode('utf-8')
 req=urllib.request.Request(url,data)
-resp=urllib.request.urlopen(req)
-respData=resp.read()
-print(respData)
+try:
+    resp=urllib.request.urlopen(req)
+    respData=resp.read()
+    print(respData)
+except urllib.error.URLError as e:
+    print('Error', e.reason)
