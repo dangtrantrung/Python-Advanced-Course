c26f62ef7aeeeb097dad2cf09767e6952d9ec869
dangtrantrung <trung280584@gmail.com>
2024-02-20 20:31:39 +0700

    OOP

---
+++ Python_OOP.py
@@ -0,0 +1,15 @@
+class SuperMan:
+    #constructor, inittailize method
+    def __init__(self,para_name,para_weapon,para_color):
+        self.Name=para_name
+        self.Weapon=para_weapon
+        self.Color=para_weapon
+SuperMan_A=SuperMan("Trung","Sword","Red")
+''' SuperMan_A.Name="Trung"
+SuperMan_A.Weapon="Sword"
+SuperMan_A.Color="Red" '''
+print("Name of SuperMan_A:",SuperMan_A.Name)
+print("Weapon of SuperMan_A:",SuperMan_A.Weapon)
+print("Color of SuperMan_A:",SuperMan_A.Color)
+# print("Color of SuperMan_A:",SuperMan_A.Age)
+
